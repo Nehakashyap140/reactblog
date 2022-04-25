6d61edbff49e5e40d5fb8015df21d05e19f590f8
Neha kashyap <97278736+Nehakashyap140@users.noreply.github.com>
2022-04-25 16:16:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+/*import Footerjoin from './Footerjoin';
+
+import Headerjoin from "./Headerjoin"
+import Joinbody from './Joinbody';*/
+import Blogbody from './Blogbody';
+
+export default function App(){
+    return(
+          <div>
+              {/* /*<Headerjoin/>
+              <Joinbody/>
+              <Footerjoin/> */}
+              <Blogbody/>
+          </div>
+    )
+};
+
---
+++ Blogbody.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Blogbody.css'
+export default function Blogbody()
+{
+    return(
+            <>  <div className="whole">
+                <div className ="heading">
+                    <h1>India is the Second-Fastest-Growing freelance market in the world, Says report</h1>
+                    <div className ="container1">
+                        <img src="https://www.rebliss.in/blogpic/freelancer.png"/>
+                        <p>India is the Second-Fastest-Growing freelance market in the world with a </p>
+                        <p>massive increase with a number of freelancers this year, according to a </p>
+                        <p>report on 'Freelancing in 2020: An Abundance of Opportunities' by payoneer.</p>
+                        <div className ="container2">
+                            <p>"Ranked as the Second-Fastest-Growing freelance market in the world;</p>
+                            <p>India's freelance economy currently consists of an estimated 15 millon</p>
+                            <p>freelancers", the report said.</p>
+                            <div className="container3">
+                                <p>The report further highlighted how the pandemic has changed the</p>
+                                <p>increased technology adoption, stating that “a permanent change in the global</p>
+                                <p>workforce is inevitable”.</p>
+
+                            </div>
+                            <div className="container4">
+                               <p>“Going forward, while there will still be space for full-time employees, more and more</p>
+                               <p>companies will seek remote and freelance workers for a wider variety of tasks, “ the</p>
+                               <p>report said.</p>
+                         </div>
+                     </div>
+                    </div>
+                </div>
+                <div className="heading2">
+                    <h1>India witnesses 46% increase in new freelancers from 2020</h1>
+                    <div className="image2">
+                   <img src="https://www.rebliss.in/blogpic/freelancer2.png" widht ="650px" height="650px"/>
+                </div>
+                </div>
+                </div>
+             </>
+    )
+        
+}
